10df7301a81f12a02efefeeb00c2c1573e976243
Heba Adel Ahmed <HebaAdelAhmedMohammed@gmail.com>
2024-10-04 05:03:29 +0300

    Commit project structure

--- TasalyFlex.DAL/obj/Debug/net8.0/TasalyFlex.DAL.AssemblyInfo.cs
+++ TasalyFlex.DAL/obj/Debug/net8.0/TasalyFlex.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasalyFlex.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f93f893a87582fce85dfda6c99b52077e15ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasalyFlex.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasalyFlex.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
